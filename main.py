150bcd802e0a93e1edb2be33144c45b5c5398a33
Vincentcolotti-12 <826617@mystma.org>
2022-09-21 14:48:09 -0500

    attempt one

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def addCheck(x,y,z):
-    if y+x == z:
-        return True
+def close10(x, y):
+    if abs(10-x) < abs(10-y):
+        return x
+    elif abs(10-x) > abs(10-y):
+        return y
     else:
-        return False
+        return 0
+
